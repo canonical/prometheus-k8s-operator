380a6c1614bfdfe665827febf73e32a00f7532de
Balbir Thomas <balbir.thomas@canonical.com>
2020-09-08 10:59:45 +0100

    Add alertmanager relation tests

--- tests/test_charm.py
+++ tests/test_charm.py
@@ -2,10 +2,24 @@
 # See LICENSE file for licensing details.
 
 import unittest
-# from unittest.mock import Mock
+import yaml
 
 from ops.testing import Harness
 from charm import PrometheusCharm
+
+MINIMAL_CONFIG = {
+    'prometheus-image-path': 'prom/prometheus',
+    'advertised-port': 9090
+}
+
+SMTP_ALERTING_CONFIG = {
+    'globals': {
+        'smtp_smarthost': 'localhost:25',
+        'smtp_from': 'alertmanager@example.org',
+        'smtp_auth_username': 'alertmanager',
+        'smtp_auth_password': 'password',
+    }
+}
 
 
 class TestCharm(unittest.TestCase):
@@ -38,3 +52,59 @@ class TestCharm(unittest.TestCase):
         missing = self.harness.charm._check_config()
         expected = ['prometheus-image-password']
         self.assertEqual(missing, expected)
+
+    def test_alerting_config_is_updated_by_alertmanager_relation(self):
+        self.harness.begin()
+        self.harness.set_leader(True)
+
+        # check alerting config is empty without alertmanager relation
+        self.harness.update_config(MINIMAL_CONFIG)
+        self.assertEqual(self.harness.charm.stored.alertmanagers, {})
+        rel_id = self.harness.add_relation('alertmanager', 'smtp')
+        self.assertIsInstance(rel_id, int)
+        self.harness.add_relation_unit(rel_id, 'smtp/0')
+        pod_spec = self.harness.get_pod_spec()
+        self.assertEqual(alerting_config(pod_spec), str())
+
+        # check alerting config is updated when a alertmanager joins
+        self.harness.update_relation_data(rel_id,
+                                          'smtp/0',
+                                          {
+                                              'alerting_config':
+                                              yaml.dump(SMTP_ALERTING_CONFIG)
+                                          })
+
+        pod_spec = self.harness.get_pod_spec()
+        self.assertEqual(alerting_config(pod_spec), SMTP_ALERTING_CONFIG)
+
+    def test_alerting_config_is_removed_when_alertmanager_departs(self):
+        self.harness.begin()
+        self.harness.set_leader(True)
+
+        # ensure there is a non-empty alerting config
+        self.harness.update_config(MINIMAL_CONFIG)
+        rel_id = self.harness.add_relation('alertmanager', 'smtp')
+        rel = self.harness.model.get_relation('alertmanager')
+        self.assertIsInstance(rel_id, int)
+        self.harness.add_relation_unit(rel_id, 'smtp/0')
+        self.harness.update_relation_data(rel_id,
+                                          'smtp/0',
+                                          {
+                                              'alerting_config':
+                                              yaml.dump(SMTP_ALERTING_CONFIG)
+                                          })
+        pod_spec = self.harness.get_pod_spec()
+        self.assertEqual(alerting_config(pod_spec), SMTP_ALERTING_CONFIG)
+
+        # check alerting config is removed when relation departs
+        self.harness.charm.on.alertmanager_relation_departed.emit(rel)
+        pod_spec = self.harness.get_pod_spec()
+        self.assertEqual(alerting_config(pod_spec), str())
+
+
+def alerting_config(pod_spec):
+    config_yaml = pod_spec[0]['containers'][0]['files'][0]['files']['prometheus.yml']
+    config_dict = yaml.safe_load(config_yaml)
+    alerting_yaml = config_dict['alerting']
+    alerting = yaml.safe_load(alerting_yaml) if alerting_yaml else str()
+    return alerting
