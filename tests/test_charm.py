68e293d1cc736c9fca027b31ca0cbcc17eab2a36
Balbir Thomas <balbir.thomas@canonical.com>
2020-11-02 12:45:22 +0000

    Review: Made all charm class members private
    This commit implements changes requested on code review by Jeremy Lounder.
    In particular all charm class members have been made private by adding
    a underscore prefix to their name.

--- tests/test_charm.py
+++ tests/test_charm.py
@@ -70,7 +70,7 @@ class TestCharm(unittest.TestCase):
 
         # check alerting config is empty without alertmanager relation
         self.harness.update_config(MINIMAL_CONFIG)
-        self.assertEqual(self.harness.charm.stored.alertmanagers, {})
+        self.assertEqual(self.harness.charm._stored.alertmanagers, {})
         rel_id = self.harness.add_relation('alertmanager', 'smtp')
         self.assertIsInstance(rel_id, int)
         self.harness.add_relation_unit(rel_id, 'smtp/0')
