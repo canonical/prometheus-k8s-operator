810e6794987cb477c4093e4dccd79cf2939b3b42
Balbir Thomas <balbir.thomas@canonical.com>
2021-05-10 11:44:27 +0100

    Documented Prometheus server
    This commit adds docstrings for the Prometheus object.

--- src/prometheus_server.py
+++ src/prometheus_server.py
@@ -5,11 +5,24 @@ import urllib3
 class Prometheus:
 
     def __init__(self, host, port):
+        """Utility to manage a Prometheus application.
+        Args:
+            host: host address of Prometheus application.
+            port: port on which Prometheus service is exposed.
+        """
         self.host = host
         self.port = port
         self.http = urllib3.PoolManager()
 
     def build_info(self):
+        """Fetch build information from Prometheus.
+
+        Returns:
+            a dictionary containing build information (for instance
+            version) of the Prometheus application. If the Prometheus
+            instance is not reachable then an empty dictionary is
+            returned.
+        """
         api_path = "api/v1/status/buildinfo"
         url = "http://{}:{}/{}".format(
             self.host,
