4ae75160b1cf04fedcdd82a3b669d53f28e18642
sed-i <82407168+sed-i@users.noreply.github.com>
2021-09-03 09:46:24 -0400

    update alertmanager lib from its repo (#115)

--- lib/charms/alertmanager_k8s/v0/alertmanager.py
+++ lib/charms/alertmanager_k8s/v0/alertmanager.py
@@ -2,19 +2,19 @@
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-""" # alertmanager library
+"""# alertmanager library.
 
 This library is designed to be used by a charm consuming or providing the `alerting` relation.
 """
 
-import ops
-from ops.framework import EventSource, EventBase
-from ops.relation import ConsumerBase, ProviderBase
-from ops.charm import CharmBase, RelationJoinedEvent, RelationEvent
-from ops.model import Relation
-
-from typing import List
 import logging
+from typing import List
+
+import ops
+from ops.charm import CharmBase, RelationEvent, RelationJoinedEvent
+from ops.framework import EventBase, EventSource
+from ops.model import Relation
+from ops.relation import ConsumerBase, ProviderBase
 
 LIBID = "abcdef1234"  # Unique ID that refers to the library forever
 LIBAPI = 0  # Must match the major version in the import path.
@@ -27,7 +27,7 @@ class ClusterChanged(EventBase):
     """Event raised when an alertmanager cluster is changed.
 
     If an alertmanager unit is added to or removed from a relation,
-    then a :class:`ClusterChanged` event is raised.
+    then a :class:`ClusterChanged` event should be emitted.
     """
 
     def __init__(self, handle, data=None):
@@ -85,14 +85,14 @@ class AlertmanagerConsumer(ConsumerBase):
         )
 
     def _on_relation_changed(self, event: ops.charm.RelationChangedEvent):
-        """This hook notifies the charm that there may have been changes to the cluster"""
+        """This hook notifies the charm that there may have been changes to the cluster."""
         if event.unit:  # event.unit may be `None` in the case of app data change
             # inform consumer about the change
             self.cluster_changed.emit()
 
     def get_cluster_info(self) -> List[str]:
-        """Returns a list of ip addresses of all the alertmanager units"""
-        alertmanagers = []
+        """Returns a list of ip addresses of all the alertmanager units."""
+        alertmanagers: List[str] = []
         if not (relation := self.charm.model.get_relation(self._consumer_relation_name)):
             return alertmanagers
         for unit in relation.units:
@@ -100,42 +100,48 @@ class AlertmanagerConsumer(ConsumerBase):
                 alertmanagers.append(address)
         return sorted(alertmanagers)
 
-    def _on_relation_departed(self, event: ops.charm.RelationDepartedEvent):
-        """This hook notifies the charm that there may have been changes to the cluster"""
+    def _on_relation_departed(self, _):
+        """This hook notifies the charm that there may have been changes to the cluster."""
         self.cluster_changed.emit()
 
-    def _on_relation_broken(self, event: ops.charm.RelationBrokenEvent):
+    def _on_relation_broken(self, _):
+        """This hook notifies the charm that a relation has been completely removed."""
         # inform consumer about the change
         self.cluster_changed.emit()
 
 
 class AlertmanagerProvider(ProviderBase):
-    """A "provider" handler to be used by the Alertmanager charm for abstracting away all the
-    communication with consumers.
+    """An alertmanager "provider" handler for abstracting away communication with consumers.
+
     This provider auto-registers relation events on behalf of the main Alertmanager charm.
 
     Arguments:
             charm (CharmBase): consumer charm
+            relation_name (str): relation name (not interface name)
             service_name (str): a name for the provided service
             consumes (dict): provider specifications
-            multi (bool): multiple relations flag
+            api_port (int): alertmanager server's api port; this is needed here to avoid accessing
+                            charm constructs directly
 
     Attributes:
             charm (CharmBase): the Alertmanager charm
     """
 
-    _provider_relation_name = "alerting"
-
-    def __init__(self, charm, service_name: str, version: str = None):
-        super().__init__(charm, self._provider_relation_name, service_name, version)
-        self.charm = charm  # TODO remove?
+    def __init__(
+        self,
+        charm,
+        relation_name: str,
+        service_name: str,
+        version: str = None,
+        api_port: int = 9093,
+    ):
+        super().__init__(charm, relation_name, service_name, version)
+        self.charm = charm
         self._service_name = service_name
 
-        # Set default value for the public port
-        # This is needed here to avoid accessing charm constructs directly
-        self._api_port = 9093  # default value
+        self._api_port = api_port
 
-        events = self.charm.on[self._provider_relation_name]
+        events = self.charm.on[self.name]
 
         # No need to observe `relation_departed` or `relation_broken`: data bags are auto-updated
         # so both events are address on the consumer side.
@@ -143,31 +149,33 @@ class AlertmanagerProvider(ProviderBase):
 
     @property
     def api_port(self):
-        """Get the API port number to use for alertmanager (default: 9093)."""
+        """Get the API port number to use for alertmanager."""
         return self._api_port
 
-    @api_port.setter
-    def api_port(self, value: int):
-        """Set the API port number to use for alertmanager (must match the provider charm)."""
-        self._api_port = value
-
     def _on_relation_joined(self, event: RelationJoinedEvent):
-        """This hook stores the public address of the newly-joined "alerting" relation in the
-        corresponding data bag.
+        """This hook stores the public address of the newly-joined "alerting" relation.
+
         This is needed for consumers such as prometheus, which should be aware of all alertmanager
         instances.
         """
         self.update_relation_data(event)
 
     def _generate_relation_data(self, relation: Relation):
-        public_address = "{}:{}".format(
-            self.model.get_binding(relation).network.bind_address, self.api_port
-        )
+        """Helper function to generate relation data in the correct format."""
+        public_address = f"{self.model.get_binding(relation).network.bind_address}:{self.api_port}"
         return {"public_address": public_address}
 
     def update_relation_data(self, event: RelationEvent = None):
-        # "ingress-address" is auto-populated incorrectly so rolling my own, "public_address"
+        """Helper function for updating relation data bags.
 
+        This function can be used in two different ways:
+        - update relation data bag of a given event (e.g. a newly joined relation);
+        - update relation data for all relations
+
+        Args:
+            event: The event whose data bag needs to be updated. If it is None, update data bags of
+            all relations.
+        """
         if event is None:
             # update all existing relation data
             # a single consumer charm's unit may be related to multiple providers
