59f977004191573e8088d92570eeaffb8db7a95d
Balbir Thomas <balbir.thomas@canonical.com>
2021-10-18 11:51:49 +0100

    Test multiple images
    This commit refactors the build and deploy test to use multiple
    images. To facilitate reuse of built Prometheus charm, this
    commit also adds a session scoped test fixture.

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -10,14 +10,15 @@ logger = logging.getLogger(__name__)
 
 
 @pytest.mark.abort_on_fail
-async def test_build_and_deploy_with_ubuntu_image(ops_test):
+@pytest.mark.parametrize("oci_image", ["ubuntu/prometheus:latest", "prom/prometheus:latest"])
+async def test_build_and_deploy_with_alternative_images(ops_test, prometheus_charm, oci_image):
     """Test that the Prometheus charm can be deployed successfully."""
-    prometheus_charm = await ops_test.build_charm(".")
-    resources = {"prometheus-image": "ubuntu/prometheus:latest"}
+    resources = {"prometheus-image": oci_image}
+    app_name = "prometheus-" + oci_image.split("/")[0]
 
-    await ops_test.model.deploy(
-        prometheus_charm, resources=resources, application_name="prometheus"
-    )
-    await ops_test.model.wait_for_idle(apps=["prometheus"], status="active")
+    await ops_test.model.deploy(prometheus_charm, resources=resources, application_name=app_name)
+    await ops_test.model.wait_for_idle(apps=[app_name], status="active")
 
-    assert ops_test.model.applications["prometheus"].units[0].workload_status == "active"
+    assert ops_test.model.applications[app_name].units[0].workload_status == "active"
+
+    await ops_test.model.applications[app_name].remove()
