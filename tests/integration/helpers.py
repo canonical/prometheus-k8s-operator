ab8fb9cf4245957fb9e6a2bd0e78b21db1b31133
Balbir Thomas <balbir.thomas@canonical.com>
2021-10-27 15:04:33 +0100

    Added integration test for basic scrape functionality
    This commit adds and integration test to check that the
    basic functionality of the prometheus_scrape interface works
    as expected. In particular
    - adding a relation updates scrape jobs and alert rules
    - removing the relation also updates scrape jobs and alert rules

---
+++ tests/integration/helpers.py
@@ -0,0 +1,110 @@
+import yaml
+from workload import Prometheus
+
+
+async def unit_address(ops_test, app_name, unit_num):
+    """Find unit address for any application.
+
+    Args:
+        ops_test: pytest-operator plugin
+        app_name: string name of application
+        unit_num: integer number of a juju unit
+
+    Returns:
+        unit address as a string
+    """
+    status = await ops_test.model.get_status()
+    return status["applications"][app_name]["units"][f"{app_name}/{unit_num}"]["address"]
+
+
+async def check_prometheus_is_ready(ops_test, app_name, unit_num):
+    """Check if Prometheus server responds to HTTP API requests.
+
+    Args:
+        ops_test: pytest-operator plugin
+        app_name: string name of Prometheus application
+        unit_num: integer number of a Prometheus juju unit
+
+    Returns:
+        True if Prometheus is responsive else False
+    """
+    host = await unit_address(ops_test, app_name, unit_num)
+    prometheus = Prometheus(host=host)
+    is_ready = await prometheus.is_ready()
+    assert is_ready
+
+
+async def get_prometheus_config(ops_test, app_name, unit_num):
+    """Fetch Prometheus configuration.
+
+    Args:
+        ops_test: pytest-operator plugin
+        app_name: string name of Prometheus application
+        unit_num: integer number of a Prometheus juju unit
+
+    Returns:
+        Prometheus YALM configuration in string format.
+    """
+    host = await unit_address(ops_test, app_name, unit_num)
+    prometheus = Prometheus(host=host)
+    config = await prometheus.config()
+    return config
+
+
+async def get_prometheus_rules(ops_test, app_name, unit_num):
+    """Fetch all Prometheus rules.
+
+    Args:
+        ops_test: pytest-operator plugin
+        app_name: string name of Prometheus application
+        unit_num: integer number of a Prometheus juju unit
+
+    Returns:
+        a list of rule groups.
+    """
+    host = await unit_address(ops_test, app_name, unit_num)
+    prometheus = Prometheus(host=host)
+    rules = await prometheus.rules()
+    return rules
+
+
+def get_job_config_for(app_name, job_config):
+    """Find scrape configuration for a specific application.
+
+    A Prometheus scrape configuration may have multiple "jobs". The
+    prometheus charm creates a separate job for each related scrape
+    target charm. This functions finds the scrape job for a particular
+    application.
+
+    Args:
+        app_name: string name of a scrape target application
+        job_config: Prometheus scrape configuration as a YAML string
+
+    Returns:
+        a dictionary (possibly empty) representing a scrape job
+    """
+    jobs = yaml.safe_load(job_config)
+    for job in jobs["scrape_configs"]:
+        if app_name in job["job_name"]:
+            return job
+    return {}
+
+
+def get_rules_for(app_name, rule_groups):
+    """Find rule group for a specific application.
+
+    Prometheus charm creates a rule group for each related scrape
+    target. This method finds the rule group for one specific
+    application.
+
+    Args:
+        app_name: string name of scrape target application
+        rule_groups: list of Prometheus rule groups
+
+    Returns:
+        a dictionary (possibly empty) representing the rule group
+    """
+    for group in rule_groups:
+        if app_name in group["name"]:
+            return group
+    return {}
--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -5,6 +5,13 @@
 import logging
 
 import pytest
+from helpers import (
+    check_prometheus_is_ready,
+    get_job_config_for,
+    get_prometheus_config,
+    get_prometheus_rules,
+    get_rules_for,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -18,10 +25,14 @@ async def test_build_and_deploy_with_alternative_images(ops_test, prometheus_cha
 
     await ops_test.model.deploy(prometheus_charm, resources=resources, application_name=app_name)
     await ops_test.model.wait_for_idle(apps=[app_name], status="active")
+    await ops_test.model.block_until(lambda: len(ops_test.model.applications[app_name].units) > 0)
 
     assert ops_test.model.applications[app_name].units[0].workload_status == "active"
 
+    await check_prometheus_is_ready(ops_test, app_name, 0)
+
     await ops_test.model.applications[app_name].remove()
+    await ops_test.model.reset()
 
 
 @pytest.mark.abort_on_fail
@@ -34,7 +45,64 @@ async def test_build_and_deploy_prometheus_tester(ops_test, prometheus_tester_ch
         prometheus_tester_charm, resources=resources, application_name=app_name
     )
     await ops_test.model.wait_for_idle(apps=[app_name], status="active")
+    await ops_test.model.block_until(lambda: len(ops_test.model.applications[app_name].units) > 0)
 
     assert ops_test.model.applications[app_name].units[0].workload_status == "active"
 
     await ops_test.model.applications[app_name].remove()
+    await ops_test.model.reset()
+
+
+@pytest.mark.abort_on_fail
+async def test_prometheus_scrape_relation_with_prometheus_tester(
+    ops_test, prometheus_charm, prometheus_tester_charm
+):
+    """Test that Prometheus tester charm can be deployed successfully."""
+    tester_resources = {"prometheus-tester-image": "python:slim"}
+    prometheus_resources = {"prometheus-image": "ubuntu/prometheus:latest"}
+    prometheus_app_name = "prometheus"
+    tester_app_name = "prometheus-tester"
+
+    await ops_test.model.deploy(
+        prometheus_charm, resources=prometheus_resources, application_name=prometheus_app_name
+    )
+    await ops_test.model.deploy(
+        prometheus_tester_charm, resources=tester_resources, application_name=tester_app_name
+    )
+    await ops_test.model.wait_for_idle(apps=[prometheus_app_name], status="active")
+    await ops_test.model.wait_for_idle(apps=[tester_app_name], status="active")
+    await ops_test.model.block_until(
+        lambda: len(ops_test.model.applications[prometheus_app_name].units) > 0
+    )
+    await ops_test.model.block_until(
+        lambda: len(ops_test.model.applications[tester_app_name].units) > 0
+    )
+
+    assert ops_test.model.applications[prometheus_app_name].units[0].workload_status == "active"
+    assert ops_test.model.applications[tester_app_name].units[0].workload_status == "active"
+
+    await check_prometheus_is_ready(ops_test, prometheus_app_name, 0)
+    initial_config = await get_prometheus_config(ops_test, prometheus_app_name, 0)
+    initial_rules = await get_prometheus_rules(ops_test, prometheus_app_name, 0)
+
+    await ops_test.model.add_relation(prometheus_app_name, tester_app_name)
+    await ops_test.model.wait_for_idle(apps=[prometheus_app_name], status="active")
+    config_with_relation = await get_prometheus_config(ops_test, prometheus_app_name, 0)
+    tester_job = get_job_config_for(tester_app_name, config_with_relation)
+    assert tester_job != {}
+
+    rules_with_relation = await get_prometheus_rules(ops_test, prometheus_app_name, 0)
+    tester_rules = get_rules_for(tester_app_name, rules_with_relation)
+    assert tester_rules != {}
+
+    await ops_test.model.applications[tester_app_name].remove()
+    await ops_test.model.wait_for_idle(apps=[prometheus_app_name], status="active")
+
+    relation_removed_config = await get_prometheus_config(ops_test, prometheus_app_name, 0)
+    assert initial_config == relation_removed_config
+
+    relation_removed_rules = await get_prometheus_rules(ops_test, prometheus_app_name, 0)
+    assert initial_rules == relation_removed_rules
+
+    await ops_test.model.applications[prometheus_app_name].remove()
+    await ops_test.model.reset()
