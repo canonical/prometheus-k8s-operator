342c80aa5efdfb35d015058c774c72caa5a33ba1
Balbir Thomas <balbir.thomas@canonical.com>
2021-10-27 15:04:33 +0100

    Added integration test for basic scrape functionality
    This commit adds and integration test to check that the
    basic functionality of the prometheus_scrape interface works
    as expected. In particular
    - adding a relation updates scrape jobs and alert rules
    - removing the relation also updates scrape jobs and alert rules

---
+++ tests/integration/workload.py
@@ -0,0 +1,53 @@
+import logging
+
+import aiohttp
+
+logger = logging.getLogger(__name__)
+
+
+class Prometheus:
+    """A class that represents a running instance of Prometheus."""
+
+    def __init__(self, host="localhost", port=9090):
+        """Utility to manage a Prometheus application.
+
+        Args:
+            host: Optional; host address of Prometheus application.
+            port: Optional; port on which Prometheus service is exposed.
+        """
+        self.base_url = f"http://{host}:{port}"
+
+    async def is_ready(self):
+        """Send a GET request to check readiness.
+
+        Returns:
+          True if Prometheus is ready (returned 200 OK); False otherwise.
+        """
+        url = f"{self.base_url}/-/ready"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                return response.status == 200
+
+    async def config(self):
+        """Send a GET request to get Prometheus configuation.
+
+        Returns:
+          YAML config in string format or empty string
+        """
+        url = f"{self.base_url}/api/v1/status/config"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                result = await response.json()
+                return result["data"]["yaml"] if result["status"] == "success" else ""
+
+    async def rules(self):
+        """Send a GET request to get Prometheus rules.
+
+        Returns:
+          Rule Groups list or empty list
+        """
+        url = f"{self.base_url}/api/v1/rules"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                result = await response.json()
+                return result["data"]["groups"] if result["status"] == "success" else []
