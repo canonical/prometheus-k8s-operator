fd2e3a3167aeb15ced01c23a3c7c358b2672dacc
Balbir Thomas <balbir.thomas@canonical.com>
2021-11-02 11:47:47 +0000

    Removed integration test with upstream image
    This commit removes integration test with upstream image as that
    was requested by code review.

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -17,11 +17,10 @@ logger = logging.getLogger(__name__)
 
 
 @pytest.mark.abort_on_fail
-@pytest.mark.parametrize("oci_image", ["ubuntu/prometheus:latest", "prom/prometheus:latest"])
-async def test_build_and_deploy_with_alternative_images(ops_test, prometheus_charm, oci_image):
+async def test_build_and_deploy_with_alternative_images(ops_test, prometheus_charm):
     """Test that the Prometheus charm can be deployed successfully."""
-    resources = {"prometheus-image": oci_image}
-    app_name = "prometheus-" + oci_image.split("/")[0]
+    resources = {"prometheus-image": "ubuntu/prometheus:latest"}
+    app_name = "prometheus-ubuntu"
 
     await ops_test.model.deploy(prometheus_charm, resources=resources, application_name=app_name)
     await ops_test.model.wait_for_idle(apps=[app_name], status="active")
