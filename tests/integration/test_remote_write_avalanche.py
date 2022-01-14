deac609e75fc547a583dbf49758e9083a440ae4e
sed-i <82407168+sed-i@users.noreply.github.com>
2022-01-14 17:00:14 -0500

    improve itest

--- tests/integration/test_remote_write_avalanche.py
+++ tests/integration/test_remote_write_avalanche.py
@@ -3,17 +3,15 @@
 # See LICENSE file for licensing details.
 
 
-import json
 import logging
-import urllib.request
 from pathlib import Path
-from typing import Optional
 
 import pytest
 import yaml
 from helpers import IPAddressWorkaround, unit_address  # type: ignore[import]
 from juju import utils
 from pytest_operator.plugin import OpsTest
+from workload import Prometheus
 
 log = logging.getLogger(__name__)
 
@@ -43,92 +41,31 @@ async def test_charm_successfully_relates_to_avalanche(ops_test: OpsTest):
 
 
 async def test_avalanche_metrics_are_ingested_by_prometheus(ops_test: OpsTest):
-    prom_url = f"http://{await unit_address(ops_test, 'prom', 0)}:9090/api/v1/labels"
+    address = await unit_address(ops_test, "prom", 0)
 
-    response = urllib.request.urlopen(prom_url, data=None, timeout=5.0)
-    assert response.code == 200
+    async def prometheus_labels_contain_avalanche_labels() -> bool:
+        return "label_key_kkkkk_0" in await Prometheus(address).labels()
 
-    # response looks like this:
-    # {
-    #   "status": "success",
-    #   "data": [
-    #     "__name__",
-    #     "alertname",
-    #     "alertstate",
-    #     ...
-    #     "juju_application",
-    #     "juju_charm",
-    #     "juju_model",
-    #     "juju_model_uuid",
-    #     "label_key_kkkkk_0",
-    #     "label_key_kkkkk_1",
-    #     "label_key_kkkkk_2",
-    #     ...
-    #     "version"
-    #   ]
-    # }
-
-    labels_response = json.loads(response.read())
-    assert "label_key_kkkkk_0" in labels_response["data"]
+    await utils.block_until_with_coroutine(prometheus_labels_contain_avalanche_labels, timeout=60)
 
 
 @pytest.mark.abort_on_fail
 async def test_avalanche_alerts_ingested_by_prometeus(ops_test: OpsTest):
-    prom_url = f"http://{await unit_address(ops_test, 'prom', 0)}:9090/api/v1/rules?type=alert"
+    address = await unit_address(ops_test, "prom", 0)
 
-    response = urllib.request.urlopen(prom_url, data=None, timeout=5.0)
-    assert response.code == 200
+    async def prometheus_alerts_contain_avalanche_alerts() -> bool:
+        return len(await Prometheus(address).rules("alert")) > 0
 
-    # response looks like this:
-    # {"status":"success","data":{"groups":[]}
-
-    alerts_response = json.loads(response.read())
-    assert len(alerts_response["data"]["groups"]) > 0
+    await utils.block_until_with_coroutine(prometheus_alerts_contain_avalanche_alerts, timeout=60)
 
 
 async def test_avalanche_always_firing_alarm_is_firing(ops_test: OpsTest):
-    async def get_alert() -> Optional[dict]:
-        prom_url = f"http://{await unit_address(ops_test, 'prom', 0)}:9090/api/v1/alerts"
+    address = await unit_address(ops_test, "prom", 0)
 
-        response = urllib.request.urlopen(prom_url, data=None, timeout=5.0)
-        assert response.code == 200
+    async def avalanche_alert_is_firing() -> bool:
+        if alerts := await Prometheus(address).alerts():
+            alert = alerts[0]  # there is only one alert
+            return alert["labels"]["alertname"] == "AlwaysFiring" and alert["state"] == "firing"
+        return False
 
-        # response looks like this:
-        #
-        # {
-        #   "status": "success",
-        #   "data": {
-        #     "alerts": [
-        #       {
-        #         "labels": {
-        #           "alertname": "AlwaysFiring",
-        #           "job": "non_existing_job",
-        #           "juju_application": "avalanche-k8s",
-        #           "juju_charm": "avalanche-k8s",
-        #           "juju_model": "remotewrite",
-        #           "juju_model_uuid": "5d2582f6-f8c9-4496-835b-675431d1fafe",
-        #           "severity": "High"
-        #         },
-        #         "annotations": {
-        #           "description": " of job non_existing_job is firing the dummy alarm.",
-        #           "summary": "Instance  dummy alarm (always firing)"
-        #         },
-        #         "state": "firing",
-        #         "activeAt": "2022-01-13T18:53:12.808550042Z",
-        #         "value": "1e+00"
-        #       }
-        #     ]
-        #   }
-        # }
-
-        alerts_response = json.loads(response.read())
-        alerts = alerts_response["data"]["alerts"]
-        if len(alerts) == 0:
-            return None
-        return alerts[0]  # there is only one alert
-
-    await utils.block_until_with_coroutine(get_alert, timeout=60)
-
-    alert = await get_alert()
-    assert alert["labels"]["alertname"] == "AlwaysFiring"
-    assert alert["state"] == "firing"
+    await utils.block_until_with_coroutine(avalanche_alert_is_firing, timeout=60)
