45a784536cb3683a2962f523895dd8e8b871b9dc
sed-i <82407168+sed-i@users.noreply.github.com>
2022-01-14 23:29:45 -0500

    use tenacity

--- tests/integration/test_remote_write_avalanche.py
+++ tests/integration/test_remote_write_avalanche.py
@@ -2,20 +2,26 @@
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-
 import logging
 from pathlib import Path
 
 import pytest
 import yaml
 from helpers import IPAddressWorkaround, unit_address  # type: ignore[import]
-from juju import utils
 from pytest_operator.plugin import OpsTest
+from tenacity import retry, stop_after_attempt, wait_exponential
 from workload import Prometheus
 
 log = logging.getLogger(__name__)
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+
+# Some tests need to be retried, for example the alert does not fire as soon as a remote-write
+# relation is formed. The same could be achieved by `utils.block_until_with_coroutine`, but the
+# disadvantage is that the log would be very vague: it would show "asyncio.exceptions.TimeoutError"
+# without pytest printing out the nicely formatted mismatch between left and right.
+# By using tenacity the test body can be kept short, and the logs descriptive.
+tenatious = retry(wait=wait_exponential(multiplier=1, min=10, max=60), stop=stop_after_attempt(7))
 
 
 @pytest.mark.abort_on_fail
@@ -40,32 +46,22 @@ async def test_charm_successfully_relates_to_avalanche(ops_test: OpsTest):
     await ops_test.model.wait_for_idle(apps=["av", "prom"], status="active")
 
 
+@tenatious
 async def test_avalanche_metrics_are_ingested_by_prometheus(ops_test: OpsTest):
     address = await unit_address(ops_test, "prom", 0)
-
-    async def prometheus_labels_contain_avalanche_labels() -> bool:
-        return "label_key_kkkkk_0" in await Prometheus(address).labels()
-
-    await utils.block_until_with_coroutine(prometheus_labels_contain_avalanche_labels, timeout=60)
+    assert "label_key_kkkkk_0" in await Prometheus(address).labels()
 
 
 @pytest.mark.abort_on_fail
+@tenatious
 async def test_avalanche_alerts_ingested_by_prometeus(ops_test: OpsTest):
     address = await unit_address(ops_test, "prom", 0)
-
-    async def prometheus_alerts_contain_avalanche_alerts() -> bool:
-        return len(await Prometheus(address).rules("alert")) > 0
-
-    await utils.block_until_with_coroutine(prometheus_alerts_contain_avalanche_alerts, timeout=60)
+    assert len(await Prometheus(address).rules("alert")) > 0
 
 
+@tenatious
 async def test_avalanche_always_firing_alarm_is_firing(ops_test: OpsTest):
     address = await unit_address(ops_test, "prom", 0)
-
-    async def avalanche_alert_is_firing() -> bool:
-        if alerts := await Prometheus(address).alerts():
-            alert = alerts[0]  # there is only one alert
-            return alert["labels"]["alertname"] == "AlwaysFiring" and alert["state"] == "firing"
-        return False
-
-    await utils.block_until_with_coroutine(avalanche_alert_is_firing, timeout=60)
+    alert = await Prometheus(address).alerts()[0]  # there is only one alert
+    assert alert["labels"]["alertname"] == "AlwaysFiring"
+    assert alert["state"] == "firing"
