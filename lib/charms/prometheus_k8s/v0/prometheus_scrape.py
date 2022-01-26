ce355f9273c796398d7b700619ea934bd246515c
Ryan Barry <ryan.barry@canonical.com>
2022-01-26 23:19:28 +0000

    Bring LIBPATCH in line with charmhub

--- lib/charms/prometheus_k8s/v0/prometheus_scrape.py
+++ lib/charms/prometheus_k8s/v0/prometheus_scrape.py
@@ -312,7 +312,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 15
+LIBPATCH = 13
 
 
 logger = logging.getLogger(__name__)
