6bf5eea6ad6631fef24875b2912d7fc8c1cb4c32
Simon Aronsson <simme@arcticbit.se>
2022-02-02 23:00:44 +0100

    verify and solve #223 with unit tests and disabling shell mode (#225)

--- lib/charms/prometheus_k8s/v0/prometheus_remote_write.py
+++ lib/charms/prometheus_k8s/v0/prometheus_remote_write.py
@@ -1095,7 +1095,7 @@ class PromqlTransformer:
             return expression
         args = [str(self.path)]
         args.extend(
-            ['--label-matcher {}="{}"'.format(key, value) for key, value in topology.items()]
+            ["--label-matcher={}={}".format(key, value) for key, value in topology.items()]
         )
 
         args.extend(["{}".format(expression)])
@@ -1104,8 +1104,6 @@ class PromqlTransformer:
             return self._exec(args)
         except Exception as e:
             logger.debug('Applying the expression failed: "{}", falling back to the original', e)
-            raise e
-
             return expression
 
     def _get_transformer_path(self) -> Optional[Path]:
@@ -1123,6 +1121,6 @@ class PromqlTransformer:
         return None
 
     def _exec(self, cmd):
-        result = subprocess.run(" ".join(cmd), check=False, stdout=subprocess.PIPE, shell=True)
+        result = subprocess.run(cmd, check=False, stdout=subprocess.PIPE)
         output = result.stdout.decode("utf-8").strip()
         return output
