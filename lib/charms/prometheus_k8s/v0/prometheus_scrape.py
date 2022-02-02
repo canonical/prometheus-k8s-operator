8c8e79e86f226b008ef1ee8c4d7217af70072738
Simon Aronsson <simme@arcticbit.se>
2022-02-02 22:17:29 +0100

    verify and solve #223 with unit tests and disabling shell mode

--- lib/charms/prometheus_k8s/v0/prometheus_scrape.py
+++ lib/charms/prometheus_k8s/v0/prometheus_scrape.py
@@ -2213,7 +2213,7 @@ class PromqlTransformer:
             return expression
         args = [str(self.path)]
         args.extend(
-            ['--label-matcher {}="{}"'.format(key, value) for key, value in topology.items()]
+            ["--label-matcher={}={}".format(key, value) for key, value in topology.items()]
         )
 
         args.extend(["{}".format(expression)])
@@ -2222,8 +2222,6 @@ class PromqlTransformer:
             return self._exec(args)
         except Exception as e:
             logger.debug('Applying the expression failed: "{}", falling back to the original', e)
-            raise e
-
             return expression
 
     def _get_transformer_path(self) -> Optional[Path]:
@@ -2241,6 +2239,6 @@ class PromqlTransformer:
         return None
 
     def _exec(self, cmd):
-        result = subprocess.run(" ".join(cmd), check=False, stdout=subprocess.PIPE, shell=True)
+        result = subprocess.run(cmd, check=False, stdout=subprocess.PIPE)
         output = result.stdout.decode("utf-8").strip()
         return output
