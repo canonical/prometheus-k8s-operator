87be10ae4dfb81171fe23130f0514c7a37aa5df9
Simon Aronsson <simme@arcticbit.se>
2022-04-20 11:51:44 +0200

    bump lib patches

--- lib/charms/prometheus_k8s/v0/prometheus_remote_write.py
+++ lib/charms/prometheus_k8s/v0/prometheus_remote_write.py
@@ -33,7 +33,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 3
+LIBPATCH = 4
 
 
 logger = logging.getLogger(__name__)
--- lib/charms/prometheus_k8s/v0/prometheus_scrape.py
+++ lib/charms/prometheus_k8s/v0/prometheus_scrape.py
@@ -334,7 +334,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 17
+LIBPATCH = 18
 
 logger = logging.getLogger(__name__)
 
