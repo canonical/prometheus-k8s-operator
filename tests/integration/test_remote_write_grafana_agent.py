1a0f00880b2f5eaad345abdc5d6c1629eaeec969
sed-i <82407168+sed-i@users.noreply.github.com>
2022-06-30 15:55:53 -0400

    Attempt to fix itests

--- tests/integration/test_remote_write_grafana_agent.py
+++ tests/integration/test_remote_write_grafana_agent.py
@@ -153,6 +153,10 @@ async def test_check_data_not_persist_on_scale_0(ops_test, prometheus_charm):
 
     logger.info("Scaling down %s to zero units", prometheus_app_name)
     await ops_test.model.applications[prometheus_app_name].scale(scale=0)
+
+    logger.info("Blocking until scaled down...")
+    await ops_test.model.wait_for_idle(apps=[prometheus_app_name], wait_for_exact_units=0, timeout=120)
+
     await ops_test.model.block_until(
         lambda: len(ops_test.model.applications[prometheus_app_name].units) == 0
     )
