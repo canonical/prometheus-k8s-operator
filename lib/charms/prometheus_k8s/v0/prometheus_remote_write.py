b0641980e7afc40bec47da9053b75214ea8fe29c
sed-i <82407168+sed-i@users.noreply.github.com>
2022-09-02 02:01:45 -0400

    Bump LIBPATCH

--- lib/charms/prometheus_k8s/v0/prometheus_remote_write.py
+++ lib/charms/prometheus_k8s/v0/prometheus_remote_write.py
@@ -35,7 +35,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 6
+LIBPATCH = 7
 
 
 logger = logging.getLogger(__name__)
