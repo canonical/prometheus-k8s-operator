07f147ac13b2fd884e6d270ebe3d05c439324ab3
Simon Aronsson <simme@arcticbit.se>
2022-09-22 10:14:46 +0200

    fix import order

--- src/charm.py
+++ src/charm.py
@@ -13,9 +13,9 @@ from urllib.parse import urlparse
 
 import yaml
 from charms.alertmanager_k8s.v0.alertmanager_dispatch import AlertmanagerConsumer
+from charms.catalogue_k8s.v0.catalogue import CatalogueConsumer, CatalogueItem
 from charms.grafana_k8s.v0.grafana_dashboard import GrafanaDashboardProvider
 from charms.grafana_k8s.v0.grafana_source import GrafanaSourceProvider
-from charms.catalogue_k8s.v0.catalogue import CatalogueItem, CatalogueConsumer
 from charms.observability_libs.v0.juju_topology import JujuTopology
 from charms.observability_libs.v0.kubernetes_compute_resources_patch import (
     K8sResourcePatchFailedEvent,
