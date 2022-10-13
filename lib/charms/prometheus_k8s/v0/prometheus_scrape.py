36004c374b44cd39d7e2c42bdd2c624264336d3d
sed-i <82407168+sed-i@users.noreply.github.com>
2022-10-13 12:12:21 +0300

    Bump LIBPATCH

--- lib/charms/prometheus_k8s/v0/prometheus_scrape.py
+++ lib/charms/prometheus_k8s/v0/prometheus_scrape.py
@@ -350,7 +350,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 23
+LIBPATCH = 24
 
 logger = logging.getLogger(__name__)
 
