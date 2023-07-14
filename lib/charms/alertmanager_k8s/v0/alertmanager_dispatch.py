91c9df7844df27dbda1cd15e258edd86b4fa614b
Leon <82407168+sed-i@users.noreply.github.com>
2023-07-14 19:44:54 -0400

    Add scheme to alertmanagers config section (#495)

--- lib/charms/alertmanager_k8s/v0/alertmanager_dispatch.py
+++ lib/charms/alertmanager_k8s/v0/alertmanager_dispatch.py
@@ -42,7 +42,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 7
+LIBPATCH = 8
 
 # Set to match metadata.yaml
 INTERFACE_NAME = "alertmanager_dispatch"
@@ -68,7 +68,7 @@ class RelationManagerBase(Object):
     """Base class that represents relation ends ("provides" and "requires").
 
     :class:`RelationManagerBase` is used to create a relation manager. This is done by inheriting
-    from :class:`RelationManagerBase` and customising the sub class as required.
+    from :class:`RelationManagerBase` and customising the subclass as required.
 
     Attributes:
         name (str): consumer's relation name
@@ -101,9 +101,7 @@ class RelationManagerBase(Object):
                     )
                 )
         except KeyError:
-            raise ValueError(
-                "Relation '{}' is not in the charm's metadata.yaml".format(relation_name)
-            )
+            raise ValueError(f"Relation '{relation_name}' is not in the charm's metadata.yaml")
 
 
 class AlertmanagerConsumer(RelationManagerBase):
@@ -158,6 +156,29 @@ class AlertmanagerConsumer(RelationManagerBase):
         self.framework.observe(
             self.charm.on[self.name].relation_changed, self._on_relation_changed
         )
+
+        # The "alertmanagers" section in prometheus takes a list of "alertmanagers", each of which
+        # has its own tls_config and a list(!) of static targets:
+        # alerting:
+        #   alertmanagers:
+        #   - path_prefix: /model-name-app-name
+        #     tls_config:
+        #       ca_file: ...
+        #     scheme: http
+        #     static_configs:
+        #     - targets:
+        #       - target1
+        #       - target2
+        #
+        # The approach we take here is:
+        # - A separate instance of this library is need for every 'alerting' relation.
+        # - Every alertmanager APP gets its own entry under the "alertmanagers" section.
+        # - This lib (alertmanager_dispatch) communicates k8s-cluster FQDN (per unit address).
+        # - All units of the same app are listed as targets under the same static_configs entry.
+        #
+        # Since prometheus has unit addresses in its config, we need to emit the "cluster-changed"
+        # event on both relation-departed and relation-broken, because on scale-down e.g. from two
+        # to one unit we'd get a relation-departed, but not a relation-broken.
         self.framework.observe(
             self.charm.on[self.name].relation_departed,
             self._on_relation_departed,
@@ -171,15 +192,29 @@ class AlertmanagerConsumer(RelationManagerBase):
             self.on.cluster_changed.emit()  # pyright: ignore
 
     def get_cluster_info(self) -> List[str]:
-        """Returns a list of ip addresses of all the alertmanager units."""
-        alertmanagers = []  # type: List[str]
-        relation = self.charm.model.get_relation(self.name)
-        if not relation:
-            return alertmanagers
+        """Returns a list of addresses of all the alertmanager units."""
+        if not (relation := self.charm.model.get_relation(self.name)):
+            return []
+
+        alertmanagers: List[str] = []
         for unit in relation.units:
             address = relation.data[unit].get("public_address")
             if address:
                 alertmanagers.append(address)
+        return sorted(alertmanagers)
+
+    def get_cluster_info_with_scheme(self) -> List[str]:
+        """Returns a list of URLs of all the alertmanager units."""
+        # FIXME: in v1 of the lib, use a dict {"url": ...} so it's extendable
+        if not (relation := self.charm.model.get_relation(self.name)):
+            return []
+
+        alertmanagers: List[str] = []
+        for unit in relation.units:
+            address = relation.data[unit].get("public_address")
+            scheme = relation.data[unit].get("scheme", "http")
+            if address:
+                alertmanagers.append(f"{scheme}://{address}")
         return sorted(alertmanagers)
 
     def _on_relation_departed(self, _):
@@ -238,16 +273,17 @@ class AlertmanagerProvider(RelationManagerBase):
         self,
         charm,
         relation_name: str = "alerting",
-        api_port: int = 9093,
+        api_port: int = 9093,  # TODO: breaking change: drop this arg
         *,
-        external_url: Optional[Callable] = None,
+        external_url: Optional[Callable] = None,  # TODO: breaking change: make this mandatory
     ):
         # TODO: breaking change: force keyword-only args from relation_name onwards
         super().__init__(charm, relation_name, RelationRole.provides)
 
-        self._external_url = external_url or (
-            lambda: "http://{}:{}".format(socket.getfqdn(), api_port)
-        )
+        # TODO: only use fqdn?
+        # We don't need to worry about the literal "http" here because the external_url arg is set
+        # by the charm. TODO: drop it after external_url becomes a mandatory arg.
+        self._external_url = external_url or (lambda: f"http://{socket.getfqdn()}:{api_port}")
 
         events = self.charm.on[self.name]
 
@@ -268,10 +304,15 @@ class AlertmanagerProvider(RelationManagerBase):
 
         Addresses are without scheme.
         """
-        # Drop the scheme
+        # FIXME when `_external_url` is an ingress URL, we have a problem: we get the same URL for
+        #  both units, because alertmanager is ingress per unit. On prometheus side we can
+        #  deduplicate so that the config file only has one entry, but ideally the
+        #  "alertmanagers.[].static_configs.targets" section in the prometheus config should list
+        #  all units.
         parsed = urlparse(self._external_url())
         return {
-            "public_address": "{}:{}{}".format(parsed.hostname, parsed.port or 80, parsed.path)
+            "public_address": f"{parsed.hostname}:{parsed.port or 80}{parsed.path}",
+            "scheme": parsed.scheme,
         }
 
     def update_relation_data(self, event: Optional[RelationEvent] = None):
@@ -288,13 +329,12 @@ class AlertmanagerProvider(RelationManagerBase):
         if event is None:
             # update all existing relation data
             # a single consumer charm's unit may be related to multiple providers
-            if self.name in self.charm.model.relations:
-                for relation in self.charm.model.relations[self.name]:
-                    # Sometimes (e.g. when an app is removed with `--force`), there is a dangling
-                    # relation, for which we get the following error:
-                    # ops.model.ModelError: b'ERROR relation 17 not found (not found)\n'
-                    # when trying to `network-get alerting`.
-                    relation.data[self.charm.unit].update(self._generate_relation_data(relation))
+            for relation in self.charm.model.relations.get(self.name, []):
+                # Sometimes (e.g. when an app is removed with `--force`), there is a dangling
+                # relation, for which we get the following error:
+                # ops.model.ModelError: b'ERROR relation 17 not found (not found)\n'
+                # when trying to `network-get alerting`.
+                relation.data[self.charm.unit].update(self._generate_relation_data(relation))
 
         else:
             # update relation data only for the newly joined relation
