60177a7e5d53174affa30c729ff561512d2f7310
sed-i <82407168+sed-i@users.noreply.github.com>
2023-08-01 10:17:06 -0400

    xfail a scenario test that has a fixme

--- tests/scenario/test_server_scheme.py
+++ tests/scenario/test_server_scheme.py
@@ -49,6 +49,7 @@ class TestServerScheme:
         command = container.layers["prometheus"].services["prometheus"].command
         assert f"--web.external-url=http://{fqdn}:9090" in command
 
+    @pytest.mark.xfail
     def test_pebble_layer_scheme_becomes_https_if_tls_relation_added(
         self, context, initial_state, fqdn
     ):
