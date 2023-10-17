c001c4b7a747d12c307e5ee5576aad88f99e58c5
Guillaume Boutry <guillaume.boutry@canonical.com>
2023-10-17 11:32:00 +0200

    Add params to allowed scrape jobs keys
    Params allows to specify HTTP query parameters to append to a
    metrics_path.

--- lib/charms/prometheus_k8s/v0/prometheus_scrape.py
+++ lib/charms/prometheus_k8s/v0/prometheus_scrape.py
@@ -362,7 +362,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 42
+LIBPATCH = 43
 
 PYDEPS = ["cosl"]
 
@@ -386,6 +386,7 @@ ALLOWED_KEYS = {
     "basic_auth",
     "tls_config",
     "authorization",
+    "params",
 }
 DEFAULT_JOB = {
     "metrics_path": "/metrics",
