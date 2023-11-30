f1b35a8d92de4cee17e8506a2705438358a36c43
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2023-11-30 15:08:31 +0000

    bump libpatch

--- lib/charms/prometheus_k8s/v1/prometheus_remote_write.py
+++ lib/charms/prometheus_k8s/v1/prometheus_remote_write.py
@@ -46,7 +46,7 @@ LIBAPI = 1
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 3
+LIBPATCH = 4
 
 PYDEPS = ["cosl"]
 
