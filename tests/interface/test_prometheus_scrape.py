959f9bc10dd375b7044db9b19f65582305c252b2
IbraAoad <Ibrahim.Awwad@canonical.com>
2024-02-13 16:52:22 +0200

    remove temp branch

--- tests/interface/test_prometheus_scrape.py
+++ tests/interface/test_prometheus_scrape.py
@@ -7,6 +7,5 @@ from interface_tester import InterfaceTester
 def test_prometheus_scrape_v0_interface(interface_tester: InterfaceTester):
     interface_tester.configure(
         interface_name="prometheus_scrape",
-        branch="prom_scrape",  # TODO: Remove when prom_scrape in CRI is merged
     )
     interface_tester.run()
