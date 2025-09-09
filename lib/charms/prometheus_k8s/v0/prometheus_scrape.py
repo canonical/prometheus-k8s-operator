4e6fb5db5d0928753fe63870d88e24d7fdce4019
Luca Bello <36242061+lucabello@users.noreply.github.com>
2025-09-09 09:20:55 +0200

    bump LIBPATCH
    Signed-off-by: Luca Bello <36242061+lucabello@users.noreply.github.com>

--- lib/charms/prometheus_k8s/v0/prometheus_scrape.py
+++ lib/charms/prometheus_k8s/v0/prometheus_scrape.py
@@ -362,7 +362,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 54
+LIBPATCH = 55
 
 # Version 0.0.53 needed for cosl.rules.generic_alert_groups
 PYDEPS = ["cosl>=0.0.53"]
