c8ebd5ad826f881f70ff069c1b24c1713a42fd96
Mateusz Kulewicz <mateusz.kulewicz@canonical.com>
2025-10-09 13:10:01 +0200

    feat: Exemplars support

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -16,8 +16,15 @@ from juju.unit import Unit
 from lightkube import Client
 from lightkube.resources.core_v1 import Pod
 from minio import Minio
+from opentelemetry import metrics
+from opentelemetry.exporter.otlp.proto.http.metric_exporter import OTLPMetricExporter
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.sdk.resources import SERVICE_NAME, SERVICE_VERSION, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.trace import format_trace_id
 from pytest_operator.plugin import OpsTest
-from tenacity import retry, stop_after_attempt, wait_exponential
+from tenacity import retry, stop_after_attempt, wait_exponential, wait_fixed
 from workload import Prometheus
 
 log = logging.getLogger(__name__)
@@ -392,3 +399,64 @@ async def get_application_ip(ops_test: OpsTest, app_name: str) -> str:
     status = await ops_test.model.get_status()
     app = status["applications"][app_name]
     return app.public_address
+
+
+async def push_to_otelcol(ops_test: OpsTest, metric_name: str) -> str:
+    """Push a metric along with a trace ID to an Opentelemetry Collector that is related to Mimir so that the exemplar can be stored in Mimir.
+
+    This block creates an exemplars by attaching a trace ID provided by the Opentelemetry SDK to a metric.
+    Please visit https://opentelemetry.io/docs/languages/python/instrumentation/ for more info on how the instrumentation works and/or how to modify it.
+    """
+    otel_url = await unit_address(ops_test, "otelcol", 0)
+    collector_endpoint = f"http://{otel_url}:4318/v1/metrics"
+
+    resource = Resource(attributes={
+        SERVICE_NAME: "service",
+        SERVICE_VERSION: "1.0.0"
+    })
+
+    otlp_exporter = OTLPMetricExporter(endpoint=collector_endpoint)
+    metric_reader = PeriodicExportingMetricReader(otlp_exporter, export_interval_millis=5000)
+    meter_provider = MeterProvider(resource=resource, metric_readers=[metric_reader])
+    metrics.set_meter_provider(meter_provider)
+    meter = metrics.get_meter("meter", "1.0.0")
+    counter = meter.create_counter(metric_name, description="A placeholder counter metric")
+    tracer_provider = TracerProvider()
+
+    with tracer_provider.get_tracer("service").start_as_current_span("generate_metrics_span") as span:
+        span_ctx = span.get_span_context()
+        trace_id = span_ctx.trace_id
+
+        trace_id_hex = format_trace_id(trace_id)
+
+        counter.add(100, {"trace_id":trace_id_hex})
+
+    return trace_id_hex
+
+
+@retry(wait=wait_fixed(20), stop=stop_after_attempt(6))
+async def query_exemplars(
+    ops_test: OpsTest, query_name: str, app: str
+):
+
+    backend_url = await unit_address(ops_test, app, 0)
+
+    response = requests.get(f"http://{backend_url}:9090/api/v1/query_exemplars", params={'query': f"{query_name}_total"})
+
+    assert response.status_code == 200
+
+    response_data = response.json()
+
+    assert response_data.get("data", []), "No exemplar data found in API."
+
+    # Check if the exemplar with the trace_id is present in the response
+    exemplars = response_data["data"][0].get("exemplars", [])
+
+    assert exemplars, "No exemplars found in returned data"
+    assert exemplars[0].get("labels", {})
+
+    # Find the `trace_id` from the first exemplar's labels
+    assert exemplars[0].get("labels").get("trace_id"), "No trace_id found in returned data"
+    trace_id = exemplars[0].get("labels").get("trace_id")
+
+    return trace_id
