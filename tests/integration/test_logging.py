3ac99d5d13be37651220586f54e1f485aef5f529
Luca Bello <36242061+lucabello@users.noreply.github.com>
2026-04-30 17:28:08 +0200

    Update tests/integration/test_logging.py
    Co-authored-by: Michael Thamm <mike.thamm@canonical.com>
    Signed-off-by: Luca Bello <36242061+lucabello@users.noreply.github.com>

--- tests/integration/test_logging.py
+++ tests/integration/test_logging.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2025 Canonical Ltd.
+# Copyright 2026 Canonical Ltd.
 # See LICENSE file for licensing details.
 
 """Integration tests for the logging (log forwarding) relation."""
