7307c58ccb20c8a35ebfef6d8fb9a7f6298538da
Sina P <55766091+sinapah@users.noreply.github.com>
2026-07-01 16:27:28 -0400

    fix: validate for correct track name (#844)

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,8 +15,8 @@ variable "channel" {
   type        = string
 
   validation {
-    condition     = startswith(var.channel, "dev/")
-    error_message = "The track of the channel must be 'dev/'. e.g. 'dev/edge'."
+    condition     = startswith(var.channel, "3.11/")
+    error_message = "The track of the channel must be '3.11/'. e.g. '3.11/edge'."
   }
 }
 
